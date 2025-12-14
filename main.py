2a485c3e3e53719995993dc6f40156180861d046
bbh <jhe27216@gmail.com>
2025-12-14 21:27:33 +0800

    add

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI,Depends
 from celery_app import add
-
+from fastapi.middleware.cors import CORSMiddleware
+import dotenv
+import os
+dotenv.load_dotenv()
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv('RABBITMQ_URL')],  # 允许的前端地址（可多个）
+    # 或者开发时临时允许所有：
+    # allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法（GET, POST 等）
+    allow_headers=["*"],  # 允许所有请求头
+)
 
 async def mydepends():
     return {'hi':'there is mydepends'}
